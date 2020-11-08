755fb185284ec560bfbf499e24a7319b973eb7f9
Amit Upadhyay <amupadhyay@qualys.com>
2020-11-08 12:55:58 +0530

    Removed redirects and copied build from speed-maths

---
+++ speed-maths/precache-manifest.f5e6e76f67fa7b0fedbbba554b51f03e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a5ae578597182301c10aef37f92386",
+    "url": "./index.html"
+  },
+  {
+    "revision": "01f9a18ff7068cea6536",
+    "url": "./static/css/main.72b06e47.chunk.css"
+  },
+  {
+    "revision": "d0246c78c0add908a767",
+    "url": "./static/js/2.0f8610ca.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0f8610ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f9a18ff7068cea6536",
+    "url": "./static/js/main.a6a2e5c2.chunk.js"
+  },
+  {
+    "revision": "e0051a7f0bfd9e489d51",
+    "url": "./static/js/runtime-main.a17f0896.js"
+  }
+]);
